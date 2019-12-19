cef39e05c65fdabb4419b49bbdf8200d64cb3a91
wangsen1110 <553579727@qq.com>
2019-12-19 15:29:43 +0800

    20191219

--- app/src/main/java/com/hbmcc/wangsen/netsupport/ui/fragment/fifth/FifthTabFragment.java
+++ app/src/main/java/com/hbmcc/wangsen/netsupport/ui/fragment/fifth/FifthTabFragment.java
@@ -1,19 +1,20 @@
 package com.hbmcc.wangsen.netsupport.ui.fragment.fifth;
 
-import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
-import android.widget.FrameLayout;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
-import com.hbmcc.wangsen.netsupport.App;
 import com.hbmcc.wangsen.netsupport.R;
 import com.hbmcc.wangsen.netsupport.adapter.ComplainAdapter;
 import com.hbmcc.wangsen.netsupport.adapter.FailureAdapter;
@@ -24,8 +25,8 @@ import com.hbmcc.wangsen.netsupport.ui.fragment.MainFragment;
 import com.hbmcc.wangsen.netsupport.ui.fragment.third.WirelessData.ThridComplainData;
 import com.hbmcc.wangsen.netsupport.ui.fragment.third.WirelessData.ThridFailureData;
 import com.hbmcc.wangsen.netsupport.ui.fragment.third.WirelessData.ThridWirelessData;
-import com.hbmcc.wangsen.netsupport.util.CustomCityData;
 import com.lljjcoder.Interface.OnCustomCityPickerItemClickListener;
+import com.lljjcoder.bean.CustomCityData;
 import com.lljjcoder.citywheel.CustomConfig;
 import com.lljjcoder.style.citycustome.CustomCityPicker;
 
@@ -37,7 +38,7 @@ import java.util.List;
 import me.yokeyword.eventbusactivityscope.EventBusActivityScope;
 import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
-public class FifthTabFragment extends BaseMainFragment {
+public class FifthTabFragment extends BaseMainFragment implements View.OnClickListener{
 
     public static FifthTabFragment fragment;
     private RecyclerView recyclerViewFragmentThridTabRecentRecord;
@@ -62,8 +63,46 @@ public class FifthTabFragment extends BaseMainFragment {
     ComplainAdapter complainAdapter;
 
     private boolean isGetData = false;
-    private List<CustomCityData> mProvinceListData = new ArrayList<>();
+
+    private TextView customeBtn;
+    private TextView resultTv;
+
+    EditText mProVisibleCountEt;
+
+    CheckBox mProCyclicCk;
+
+    CheckBox mCityCyclicCk;
+
+    CheckBox mAreaCyclicCk;
+
+    CheckBox mHalfBgCk;
+    TextView mResetSettingTv;
+
+    TextView mOneTv;
+
+    TextView mTwoTv;
+
+    TextView mThreeTv;
+
+    LinearLayout pro_cyclic_ll;
+    LinearLayout city_cyclic_ll;
+    LinearLayout area_cyclic_ll;
+
+    private int visibleItems = 5;
+
+    private boolean isProvinceCyclic = true;
+
+    private boolean isCityCyclic = true;
+
+    private boolean isDistrictCyclic = true;
+
+    private boolean isShowBg = true;
     private CustomCityPicker customCityPicker = null;
+    /**
+     * 自定义数据源-省份数据
+     */
+    public List<CustomCityData> mProvinceListData = new ArrayList<>();
+    public CustomConfig.WheelType mWheelType = CustomConfig.WheelType.PRO_CITY_DIS;
 
 
     public static FifthTabFragment newInstance() {
@@ -80,6 +119,10 @@ public class FifthTabFragment extends BaseMainFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fifth_tab, container,
                 false);
+
+
+
+
         initView(view);
         return view;
     }
@@ -92,91 +135,10 @@ public class FifthTabFragment extends BaseMainFragment {
 //        recyclerViewFragmentThridTabRecentRecordcomplain = view.findViewById(R.id.recyclerView_fragment_third_tab_recent_complain);
 
 
-        CustomCityData jsPro = new CustomCityData("10000", "江苏省");
 
-        CustomCityData ycCity = new CustomCityData("11000", "盐城市");
-        List<CustomCityData> ycDistList = new ArrayList<>();
-        ycDistList.add(new CustomCityData("11100", "滨海县"));
-        ycDistList.add(new CustomCityData("11200", "阜宁县"));
-        ycDistList.add(new CustomCityData("11300", "大丰市"));
-        ycDistList.add(new CustomCityData("11400", "盐都区"));
-        ycCity.setList(ycDistList);
-
-        CustomCityData czCity = new CustomCityData("12000", "常州市");
-        List<CustomCityData> czDistList = new ArrayList<>();
-        czDistList.add(new CustomCityData("12100", "新北区"));
-        czDistList.add(new CustomCityData("12200", "天宁区"));
-        czDistList.add(new CustomCityData("12300", "钟楼区"));
-        czDistList.add(new CustomCityData("12400", "武进区"));
-        czCity.setList(czDistList);
-
-        List<CustomCityData> jsCityList = new ArrayList<>();
-        jsCityList.add(ycCity);
-        jsCityList.add(czCity);
-
-        jsPro.setList(jsCityList);
-
-
-        CustomCityData zjPro = new CustomCityData("20000", "浙江省");
-
-        CustomCityData nbCity = new CustomCityData("21000", "宁波市");
-        List<CustomCityData> nbDistList = new ArrayList<>();
-        nbDistList.add(new CustomCityData("21100", "海曙区"));
-        nbDistList.add(new CustomCityData("21200", "鄞州区"));
-        nbCity.setList(nbDistList);
-
-        CustomCityData hzCity = new CustomCityData("22000", "杭州市");
-        List<CustomCityData> hzDistList = new ArrayList<>();
-        hzDistList.add(new CustomCityData("22100", "上城区"));
-        hzDistList.add(new CustomCityData("22200", "西湖区"));
-        hzDistList.add(new CustomCityData("22300", "下沙区"));
-        hzCity.setList(hzDistList);
-
-        List<CustomCityData> zjCityList = new ArrayList<>();
-        zjCityList.add(hzCity);
-        zjCityList.add(nbCity);
-
-        zjPro.setList(zjCityList);
-
-
-        CustomCityData gdPro = new CustomCityData("30000", "广东省");
-
-        CustomCityData fjCity = new CustomCityData("21000", "潮州市");
-        List<CustomCityData> fjDistList = new ArrayList<>();
-        fjDistList.add(new CustomCityData("21100", "湘桥区"));
-        fjDistList.add(new CustomCityData("21200", "潮安区"));
-        fjCity.setList(fjDistList);
-
-
-
-        CustomCityData gzCity = new CustomCityData("22000", "广州市");
-        List<CustomCityData> szDistList = new ArrayList<>();
-        szDistList.add(new CustomCityData("22100", "荔湾区"));
-        szDistList.add(new CustomCityData("22200", "增城区"));
-        szDistList.add(new CustomCityData("22300", "从化区"));
-        szDistList.add(new CustomCityData("22400", "南沙区"));
-        szDistList.add(new CustomCityData("22500", "花都区"));
-        szDistList.add(new CustomCityData("22600", "番禺区"));
-        szDistList.add(new CustomCityData("22700", "黄埔区"));
-        szDistList.add(new CustomCityData("22800", "白云区"));
-        szDistList.add(new CustomCityData("22900", "天河区"));
-        szDistList.add(new CustomCityData("22110", "海珠区"));
-        szDistList.add(new CustomCityData("22120", "越秀区"));
-        gzCity.setList(szDistList);
-
-        List<CustomCityData> gdCityList = new ArrayList<>();
-        gdCityList.add(gzCity);
-        gdCityList.add(fjCity);
-
-        gdPro.setList(gdCityList);
-
-        mProvinceListData.add(jsPro);
-        mProvinceListData.add(zjPro);
-        mProvinceListData.add(gdPro);
 
 
     }
-
 
 
 
@@ -195,33 +157,6 @@ public class FifthTabFragment extends BaseMainFragment {
     @Override
     public void onLazyInitView(@Nullable Bundle savedInstanceState) {
         super.onLazyInitView(savedInstanceState);
-
-
-        CustomConfig cityConfig = new CustomConfig.Builder()
-                .title("选择城市")
-                .visibleItemsCount(5)
-                .setCityData(mProvinceListData)//设置数据源
-                .provinceCyclic(false)
-                .cityCyclic(false)
-                .districtCyclic(false)
-                .build();
-
-        customCityPicker = new CustomCityPicker(App.getContext());
-        customCityPicker.setCustomConfig(cityConfig);
-        customCityPicker.setOnCustomCityPickerItemClickListener(new OnCustomCityPickerItemClickListener() {
-            @Override
-            public void onSelected(CustomCityData province, CustomCityData city, CustomCityData district) {
-                if (province != null && city != null && district != null) {
-                    resultTv.setText("province：" + province.getName() + "    " + province.getId() + "\n" +
-                            "city：" + city.getName() + "    " + city.getId() + "\n" +
-                            "area：" + district.getName() + "    " + district.getId() + "\n");
-                }else{
-                    resultTv.setText("结果出错！");
-                }
-            }
-        });
-        customCityPicker.showCityPicker();
-
 
     }
 
@@ -260,4 +195,28 @@ public class FifthTabFragment extends BaseMainFragment {
         EventBusActivityScope.getDefault(_mActivity).unregister(this);
     }
 
+    @Override
+    public void onClick(View v) {
+//        if (v.getId() == R.id.btn_Time && pvTime != null) {
+//            // pvTime.setDate(Calendar.getInstance());
+//            /* pvTime.show(); //show timePicker*/
+//            pvTime.show(v);//弹出时间选择器，传递参数过去，回调的时候则可以绑定此view
+//        } else if (v.getId() == R.id.btn_Options && pvOptions != null) {
+//            pvOptions.show(); //弹出条件选择器
+//        } else if (v.getId() == R.id.btn_CustomOptions && pvCustomOptions != null) {
+//            pvCustomOptions.show(); //弹出自定义条件选择器
+//        } else if (v.getId() == R.id.btn_CustomTime && pvCustomTime != null) {
+//            pvCustomTime.show(); //弹出自定义时间选择器
+//        } else if (v.getId() == R.id.btn_no_linkage && pvNoLinkOptions != null) {//不联动数据选择器
+//            pvNoLinkOptions.show();
+//        } else if (v.getId() == R.id.btn_GotoJsonData) {//跳转到 省市区解析示例页面
+//            startActivity(new Intent(MainActivity.this, JsonDataActivity.class));
+//        } else if (v.getId() == R.id.btn_fragment) {//跳转到 fragment
+//            startActivity(new Intent(MainActivity.this, FragmentTestActivity.class));
+//        } else if (v.getId() == R.id.btn_lunar) {
+//            pvCustomLunar.show();
+//        } else if (v.getId() == R.id.btn_circle) {
+//            startActivity(new Intent(MainActivity.this, TestCircleWheelViewActivity.class));
+//        }
+    }
 }
